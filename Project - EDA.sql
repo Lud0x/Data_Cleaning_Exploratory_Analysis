c174fe1f76713f6d690a46218925a080f02cbbfc
Vivek <viveknegi212@gmail.com>
2024-07-12 00:50:11 +0530

    Update Project - EDA.sql

--- Project - EDA.sql
+++ Project - EDA.sql
@@ -2,19 +2,13 @@
 
 -- Here we are jsut going to explore the data and find trends or patterns or anything interesting like outliers
 
--- normally when you start the EDA process you have some idea of what you're looking for
-
--- with this info we are just going to look around and see what we find!
 
 SELECT * 
 FROM world_layoffs.layoffs_staging2;
 
--- EASIER QUERIES
 
 SELECT MAX(total_laid_off)
 FROM world_layoffs.layoffs_staging2;
-
-
 
 
 
@@ -35,24 +29,10 @@ SELECT *
 FROM world_layoffs.layoffs_staging2
 WHERE  percentage_laid_off = 1
 ORDER BY funds_raised_millions DESC;
--- BritishVolt looks like an EV company, Quibi! I recognize that company - wow raised like 2 billion dollars and went under - ouch
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
--- SOMEWHAT TOUGHER AND MOSTLY USING GROUP BY--------------------------------------------------------------------------------------------------
 
 -- Companies with the biggest single Layoff
 
@@ -107,9 +87,7 @@ ORDER BY 2 DESC;
 
 
 
--- TOUGHER QUERIES------------------------------------------------------------------------------------------------------------------------------------
 
--- Earlier we looked at Companies with the most Layoffs. Now let's look at that per year. It's a little more difficult.
 -- I want to look at 
 
 WITH Company_Year AS 
